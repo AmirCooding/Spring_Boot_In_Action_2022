74669e6e13d5ce4c9b632a6b65fef033aa5e6aca
Amir <Amir_Lotfi>
2022-11-19 13:49:17 +0100

    change Home Controller to WebMvcConfig

--- taco_colud_dataWorking_chapter_03/src/main/java/com/AmirCoding/taco_cloud_dataWorking_chapter_03/config/HomeViewControllerConfig.java
+++ taco_colud_dataWorking_chapter_03/src/main/java/com/AmirCoding/taco_cloud_dataWorking_chapter_03/config/HomeViewControllerConfig.java
@@ -1,11 +1,15 @@
 package com.AmirCoding.taco_cloud_dataWorking_chapter_03.config;
 
-//@Configuration
-//public class HomeViewControllerConfig implements WebMvcConfigurer {
-//
-//	@Override
-//	public void addViewControllers(ViewControllerRegistry registry) {
-//		registry.addViewController("/").setViewName("home");
-//	}
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//}
+@Configuration
+public class HomeViewControllerConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("home");
+	}
+
+}
